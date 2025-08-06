2752fbe5318284e6e13990dda7287989a563d227
sohil <sohil.khan@saint-gobain.com>
2025-08-06 21:03:52 +0530

    TRANSACTIONCODE

--- psycopg2@.py
+++ psycopg2@.py
@@ -1,2 +1,28 @@
 import psycopg2
 conn=psycopg2.connect(dbname='CSV_DATA',user='postgres',password=1234,host='localhost',port=5432)
+cursor=conn.cursor()
+cursor.execute("SELECT * FROM transactions")
+data=cursor.fetchall()
+for x in data:
+    data=list(x)
+    transaction_id = data[0]
+    customer_name = data[1]
+    customer_email = data[2]
+    product_name = data[3]
+    product_category = data[4]
+    amount = data[5]
+    transaction_date = data[6]
+    insert_query = """
+          INSERT INTO transactions2 (
+              transaction_id, customer_name, customer_email, product_name, 
+              product_category, amount, transaction_date
+          ) VALUES (%s, %s, %s, %s, %s, %s, %s);
+          """
+
+    record_to_insert = (
+        transaction_id, customer_name, customer_email, product_name,
+        product_category, amount, transaction_date
+    )
+    cursor.execute(insert_query, record_to_insert)
+    conn.commit()
+    print(f"Record for transaction_id {transaction_id} inserted successfully.")
